69d1987a8b5bab36843a8f3aca4363cf032f6b15
enowuigrek <enowuigrek@gmail.com>
2026-02-18 19:35:02 +0100

    feat: add company name and NIP to customer profile with private/business tabs

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -6,7 +6,10 @@ export const COUNTRY_COLORS = {
   Brazylia: '#8DB600',
   Kolumbia: '#FFD200',
   Honduras: '#2C5AA0',
-  Indie: '#FF8A3D'
+  Indie: '#FF8A3D',
+  Gwatemala:'#87aade',
+  Etiopia: '#ff5555',
+  Kenia: '#a02c2c',
 
 };
 
